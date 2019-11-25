bcf60dac7f34931ec75f663aff1d5b2de84cb7a6
ajithcravi <craviajith@gmail.com>
2019-11-25 15:38:23 +0530

    result card added

--- weather-app/src/components/form.js
+++ weather-app/src/components/form.js
@@ -11,7 +11,7 @@ const Form = () => {
           <input
             name="location"
             type="text"
-            className="form-control"
+            className="form-control inputBox--border-round"
             id="location"
             aria-describedby="emailHelp"
             placeholder="e.g. Chennai"
@@ -21,7 +21,20 @@ const Form = () => {
           Submit
         </button>
       </form>
-      {/* <img src="..." className="card-img-top" alt="..." /> */}
+      <div className="card mb-3 resultCard">
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <img src="..." className="card-img" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Temperature:</p>
+              <p className="card-text">Humidity:</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
