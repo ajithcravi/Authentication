d2bbea90dc146b6fbad8410bcee510ab8e382f59
ajithcravi <craviajith@gmail.com>
2019-11-25 11:27:58 +0530

    form added

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Heading from "./components/heading";
+import Form from "./components/form";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Heading />
+        <Form />
       </div>
     );
   }
