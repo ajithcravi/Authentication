de2084a42f053564fc04eb09e763fa8ccd7f05d5
ajithcravi <craviajith@gmail.com>
2019-11-25 12:52:38 +0530

    form component made with inputs

--- weather-app/src/components/heading.js
+++ weather-app/src/components/heading.js
@@ -1,9 +1,4 @@
 import React, { Component } from "react";
 
-const Heading = () => (
-  <div>
-    <h1>Hello Ajith</h1>
-    <p>Your accurate Weather App</p>
-  </div>
-);
+const Heading = () => <h1>Hello Ajith</h1>;
 export default Heading;
