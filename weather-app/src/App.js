2b7e46149cba5769df98b0800dc19b6760ff7079
ajithcravi <craviajith@gmail.com>
2019-11-26 00:56:20 +0530

    collected country code details

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,14 +4,30 @@ import Heading from "./components/heading";
 import Form from "./components/form";
 import Result from "./components/result";
 import "./App.css";
+import key from "./private/keys";
 
 class App extends React.Component {
   state = {
     displayResult: false
   };
 
-  getResult = event => {
+  getResult = async event => {
     event.preventDefault();
+    const country = event.target.elements.country.value;
+    const state = event.target.elements.state.value;
+    const city = event.target.elements.city.value;
+
+    const fetchCountryCode = await fetch(
+      `https://restcountries.eu/rest/v2/name/${country}?fullText=true`
+    );
+    const countryData = await fetchCountryCode.json();
+    const countryCode = countryData[0].alpha2Code;
+
+    const fetchWeather = await fetch(
+      `api.openweathermap.org/data/2.5/weather?q=${country},${countryCode}&units=imperial&appid=${key.weather}`
+    );
+    const weatherData = await fetchCountryCode.json();
+
     this.setState({
       displayResult: true
     });
